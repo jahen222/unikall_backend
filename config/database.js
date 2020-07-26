013a0496974166579f24c16784c8de8f0474981d
Henry Jaimes <henryjaimes.peli@gmail.com>
2020-07-26 16:56:28 -0400

    fix the credentials to aws/mysql

--- config/database.js
+++ config/database.js
@@ -5,40 +5,25 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: "${process.env.DATABASE_HOST || 'unikall.cyiqhphpjo7v.ca-central-1.rds.amazonaws.com'}",
-        port: "${process.env.DATABASE_PORT || 3306}",
-        database: "${process.env.DATABASE_NAME || 'unikall'}",
-        username: "${process.env.DATABASE_USERNAME || 'nokoarts'}",
-        password: "${process.env.DATABASE_PASSWORD || 'n0k04rt5'}",
-        ssl: "${process.env.DATABASE_SSL || 'false'}"
+        //host: "${process.env.DATABASE_HOST || '127.0.0.1'}",
+        //port: "${process.env.DATABASE_PORT || 3306}",
+        //database: "${process.env.DATABASE_NAME || 'unikall'}",
+        //username: "${process.env.DATABASE_USERNAME || 'nokoarts'}",
+        //password: "${process.env.DATABASE_PASSWORD || 'n0k04rt5'}",
+        //ssl: "${process.env.DATABASE_SSL || 'false'}"
         //host: env('DATABASE_HOST', '127.0.0.1'),
         //port: env.int('DATABASE_PORT', 3306),
         //database: env('DATABASE_NAME', 'unikall'),
         //username: env('DATABASE_USERNAME', 'nokoarts'),
         //password: env('DATABASE_PASSWORD', 'n0k04rt5'),
         //ssl: env.bool('DATABASE_SSL', false),
+        host: 'unikall.cyiqhphpjo7v.ca-central-1.rds.amazonaws.com',
+        port: 3306,
+        database: 'unikall',
+        username: 'nokoarts',
+        password: 'n0k04rt5',
       },
       options: {}
     },
   },
 });
-
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'mysql',
-        host: "${process.env.DATABASE_HOST || 'unikall.cyiqhphpjo7v.ca-central-1.rds.amazonaws.com'}",
-        port: "${process.env.DATABASE_PORT || 3306}",
-        database: "${process.env.DATABASE_NAME || 'unikall'}",
-        username: "${process.env.DATABASE_USERNAME || 'nokoarts'}",
-        password: "${process.env.DATABASE_PASSWORD || 'n0k04rt5'}",
-        ssl: "${process.env.DATABASE_SSL || 'false'}"
-      },
-      options: {}
-    },
-  },
-});
-
